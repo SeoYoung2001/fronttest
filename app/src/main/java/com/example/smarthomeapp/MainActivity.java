7b0922e7f3b06268d4ad389e567eabe2da46c804
Seoyoung Park <ghok111@naver.com>
2024-05-07 22:27:45 +0900

    commit test

--- app/src/main/java/com/example/smarthomeapp/MainActivity.java
+++ app/src/main/java/com/example/smarthomeapp/MainActivity.java
@@ -1,23 +1,58 @@
 package com.example.smarthomeapp;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
+    SwitchCompat switchCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_light);
+
+        switchCompat =findViewById(R.id.switch1);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+        MaterialButton login_btn =(MaterialButton) findViewById(R.id.login_btn);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("hi") && password.getText().toString().equals("hi")){
+                    Toast.makeText(MainActivity.this, "Login successfull",Toast.LENGTH_SHORT).show();
+
+                }else
+                    Toast.makeText(MainActivity.this, "Login fail",Toast.LENGTH_SHORT).show();
 
 
+            }
+
+        });
+
+        switchCompat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(switchCompat.isChecked()){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+                else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+
+
+            }
+        });
 
 
     }
-
-
-
 }
