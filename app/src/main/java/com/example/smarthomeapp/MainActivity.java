5baf3d0e9cac82bf18b5af49cdcca6632d31925b
Seoyoung Park <ghok111@naver.com>
2024-05-11 23:12:10 +0900

    commit test

--- app/src/main/java/com/example/smarthomeapp/MainActivity.java
+++ app/src/main/java/com/example/smarthomeapp/MainActivity.java
@@ -3,56 +3,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-    SwitchCompat switchCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
         setContentView(R.layout.activity_light);
+        setContentView(R.layout.activity_door);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lcd);
+        setContentView(R.layout.activity_sensor);
 
-        switchCompat =findViewById(R.id.switch1);
-
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
-        MaterialButton login_btn =(MaterialButton) findViewById(R.id.login_btn);
-
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(username.getText().toString().equals("hi") && password.getText().toString().equals("hi")){
-                    Toast.makeText(MainActivity.this, "Login successfull",Toast.LENGTH_SHORT).show();
-
-                }else
-                    Toast.makeText(MainActivity.this, "Login fail",Toast.LENGTH_SHORT).show();
-
-
-            }
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.switchBtn), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
-
-        switchCompat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(switchCompat.isChecked()){
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                }
-                else{
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                }
-
-
-            }
-        });
-
 
     }
 }
