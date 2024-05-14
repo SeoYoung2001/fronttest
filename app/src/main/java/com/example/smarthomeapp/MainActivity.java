a3e085e91eda5fc99a1ff46f64a29a89bcf84c19
Seoyoung Park <ghok111@naver.com>
2024-05-15 00:14:05 +0900

    commit test

--- app/src/main/java/com/example/smarthomeapp/MainActivity.java
+++ app/src/main/java/com/example/smarthomeapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.smarthomeapp;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -9,26 +12,87 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import android.widget.ImageButton;
+import android.content.Intent;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button6;
+    Button button7;
+    Button button8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_light);
-        setContentView(R.layout.activity_door);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_lcd);
-        setContentView(R.layout.activity_sensor);
+        button1 = findViewById(R.id.Temp_Btn);
+        button2 = findViewById(R.id.Door_Btn);
+        button3 = findViewById(R.id.Light_Btn);
+        button4 = findViewById(R.id.Sensor_Btn);
+        button6 = findViewById(R.id.Lcd_Btn);
+        button7 = findViewById(R.id.Hudminity_Btn);
+        button8= findViewById(R.id.imgBtn);
+        setContentView(R.layout.activity_door);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.switchBtn), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_temp);
+
+
+            }
         });
 
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_door);
+
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_light);
+
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_sensor);
+
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_lcd);
+
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_temp);
+
+            }
+        });
+
+
+
+
+
     }
+
+
 }
